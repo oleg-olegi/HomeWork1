a7d3e1a917818147603bf5ef5b84cfd766e6018f
User <ol.shink87@gmail.com>
2023-06-15 15:41:17 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Привет, мир!");
+        /*немного сложно следовать указаниям теоретической части урока
+        из-за различий версий среды разработки */
     }
 }
